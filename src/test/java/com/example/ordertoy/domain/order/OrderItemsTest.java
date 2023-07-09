787e92a50406c83b65ed399c9efa8847f4936235
Ting-Kim <taeho916@gmail.com>
2023-07-09 20:54:37 +0900

    test: OrderItems 생성 테스트 추가

--- src/test/java/com/example/ordertoy/domain/order/OrderItemsTest.java
+++ src/test/java/com/example/ordertoy/domain/order/OrderItemsTest.java
@@ -12,7 +12,7 @@ class OrderItemsTest {
 
     @Test
     @DisplayName("주문 내역에 서로 다른 가게의 메뉴가 존재하면 예외를 발생시킨다")
-    void Should_ThrowException_WhenAnotherOrderItemsOfMarkets() {
+    void Should_ThrowException_WhenOrderItemsOfAnotherMarkets() {
         // given
         Food chicken = Food.of("치킨", 18000, 90);
         Food friedPotato = Food.of("감자튀김", 5000, 30);
@@ -27,5 +27,22 @@ class OrderItemsTest {
         Assertions.assertThatThrownBy(() -> OrderItems.from(List.of(orderItem1, orderItem2, orderItem3)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("한 가게");
+    }
+
+    @Test
+    @DisplayName("주문 내역에 한 가게의 메뉴들이 담기면 정상적으로 생성된다")
+    void Should_Pass_WhenOrderItemsOfSameMarkets() {
+        // given
+        Food chicken = Food.of("치킨", 18000, 90);
+        Food friedPotato = Food.of("감자튀김", 5000, 30);
+        Food coke = Food.of("콜라", 3000, 5);
+        Market blueChicken = Market.of("파랑통닭", "판교로228번길 18 101호");
+        OrderItem orderItem1 = OrderItem.of(chicken, 1, blueChicken);
+        OrderItem orderItem2 = OrderItem.of(friedPotato, 1, blueChicken);
+        OrderItem orderItem3 = OrderItem.of(coke, 3, blueChicken);
+
+        // when & then
+        Assertions.assertThat(OrderItems.from(List.of(orderItem1, orderItem2, orderItem3)))
+                .isNotNull();
     }
 }
