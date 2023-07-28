9ade1917b38059c256c581c65af260d3b72867f6
Ting-Kim <59888684+Ting-Kim@users.noreply.github.com>
2023-07-28 18:48:18 +0900

    chore: OrderCommandService validate 메서드 추가

--- src/main/java/com/example/ordertoy/domain/order/service/OrderCommandService.java
+++ src/main/java/com/example/ordertoy/domain/order/service/OrderCommandService.java
@@ -2,6 +2,7 @@ package com.example.ordertoy.domain.order.service;
 
 import com.example.ordertoy.domain.order.Order;
 import com.example.ordertoy.domain.order.OrderCreateCommand;
+import com.example.ordertoy.domain.order.OrderValidateCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -14,5 +15,13 @@ public class OrderCommandService {
                         command.getCustomer(),
                         command.getDeliveryAddress(),
                         command.getMarket());
+  }
+
+  public Order validateOrder(OrderValidateCommand command) {
+    Order order = command.getOrder();
+
+    order.validate();
+
+    return order;
   }
 }
