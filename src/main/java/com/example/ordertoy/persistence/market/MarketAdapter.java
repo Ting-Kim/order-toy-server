dbe78cde10947d36c00e92af6f10c64e6b55cd79
Ting-Kim <taeho916@gmail.com>
2023-08-31 00:16:13 +0900

    [chore] MarketAdapter, MarketMapper 구현

---
+++ src/main/java/com/example/ordertoy/persistence/market/MarketAdapter.java
@@ -0,0 +1,63 @@
+package com.example.ordertoy.persistence.market;
+
+import com.example.ordertoy.domain.market.Market;
+import com.example.ordertoy.persistence.customer.CustomerJpaEntity;
+import com.example.ordertoy.persistence.customer.CustomerJpaRepository;
+import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MarketAdapter {
+
+  private final MarketJpaRepository marketRepository;
+  private final MarketAddressJpaRepository marketAddressRepository;
+  private final FoodSaleJpaRepository foodSaleRepository;
+  private final FoodJpaRepository foodRepository;
+  private final DeliveryPolicyJpaRepository deliveryPolicyRepository;
+  private final DeliveryTipStandardJpaRepository deliveryTipStandardRepository;
+  private final DeliveryAreaJpaRepository deliveryAreaRepository;
+  private final CustomerJpaRepository customerRepository;
+  private final MarketMapper marketMapper;
+
+  public Market findById(UUID marketId) {
+    MarketJpaEntity market = marketRepository.findById(marketId)
+                                             .orElseThrow(EntityNotFoundException::new);
+    MarketAddressJpaEntity marketAddress =
+        marketAddressRepository.findByMarketId(marketId)
+                               .orElseThrow(EntityNotFoundException::new);
+    Map<UUID, FoodJpaEntity> foodById = foodRepository.findByMarketId(marketId)
+                                                     .stream()
+                                                     .collect(Collectors.toMap(FoodJpaEntity::getId,
+                                                                               f -> f,
+                                                                               (f1, f2) -> f1));
+    List<FoodSaleJpaEntity> foodSales = foodSaleRepository.findAllById(foodById.keySet());
+
+    DeliveryPolicyJpaEntity deliveryPolicy =
+        deliveryPolicyRepository.findByMarketId(marketId)
+                                .orElseThrow(EntityNotFoundException::new);
+
+    List<DeliveryTipStandardJpaEntity> deliveryTipStandards =
+        deliveryTipStandardRepository.findAllByMarketId(marketId);
+
+    List<DeliveryAreaJpaEntity> deliveryAreas = deliveryAreaRepository.findAllById(
+        deliveryPolicy.getAvailableDeliveryAreaIds());
+
+    List<CustomerJpaEntity> customers = customerRepository.findAllById(market.getBlackCustomerIds());
+
+    return marketMapper.mapToDomain(market,
+                                    marketAddress,
+                                    foodSales,
+                                    foodById,
+                                    deliveryPolicy,
+                                    deliveryTipStandards,
+                                    deliveryAreas,
+                                    customers);
+
+  }
+}
---
+++ src/main/java/com/example/ordertoy/persistence/market/MarketMapper.java
@@ -0,0 +1,65 @@
+package com.example.ordertoy.persistence.market;
+
+import com.example.ordertoy.domain.market.BlackCustomers;
+import com.example.ordertoy.domain.market.DeliveryArea;
+import com.example.ordertoy.domain.market.DeliveryPolicy;
+import com.example.ordertoy.domain.market.DeliveryTipStandard;
+import com.example.ordertoy.domain.market.Food;
+import com.example.ordertoy.domain.market.FoodSale;
+import com.example.ordertoy.domain.market.Market;
+import com.example.ordertoy.domain.market.MarketAddress;
+import com.example.ordertoy.domain.market.Sales;
+import com.example.ordertoy.persistence.customer.CustomerJpaEntity;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MarketMapper {
+
+  Market mapToDomain(MarketJpaEntity market,
+                     MarketAddressJpaEntity marketAddress,
+                     List<FoodSaleJpaEntity> foodSales,
+                     Map<UUID, FoodJpaEntity> foodById,
+                     DeliveryPolicyJpaEntity deliveryPolicy,
+                     List<DeliveryTipStandardJpaEntity> deliveryTipStandards,
+                     List<DeliveryAreaJpaEntity> deliveryAreas,
+                     List<CustomerJpaEntity> customers) {
+    return Market.of(market.getId(),
+                     market.getName(),
+                     MarketAddress.of(marketAddress.getId(),
+                                      marketAddress.getAddress(),
+                                      marketAddress.getDetailAddress(),
+                                      marketAddress.getZipCode()),
+                     Sales.of(market.getId(),
+                              foodSales.stream()
+                                       .map(s -> {
+                                         FoodJpaEntity food = foodById.get(s.getFoodId());
+                                         return FoodSale.of(Food.of(food.getId(),
+                                                                    market.getId(),
+                                                                    food.getName(),
+                                                                    food.getPrice(),
+                                                                    food.getAverageMinuteRequired()),
+                                                            s.getSalesCount());
+                                       })
+                                       .collect(Collectors.toList())),
+                     DeliveryPolicy.of(deliveryPolicy.getId(),
+                                       deliveryPolicy.getMarketId(),
+                                       deliveryPolicy.getMinimumOrderPrice(),
+                                       deliveryTipStandards.stream()
+                                           .map(t -> DeliveryTipStandard.of(t.getOrderPriceMoreThan(),
+                                                                            t.getOrderPriceUnder(),
+                                                                            t.getDeliveryPrice()))
+                                           .collect(Collectors.toList()),
+                                       deliveryAreas.stream()
+                                                    .map(a -> DeliveryArea.of(a.getId(),
+                                                                              a.getDistrict()))
+                                                    .collect(Collectors.toList())),
+                     BlackCustomers.from(customers.stream()
+                                                  .map(CustomerJpaEntity::getId)
+                                                  .collect(Collectors.toList())));
+
+  }
+}
